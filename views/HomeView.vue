124616dd7c1915b6c1670e01591e2054931e064e
laurale31 <85873198+laurale31@users.noreply.github.com>
2023-10-25 14:38:56 -0400

    Add files via upload

---
+++ views/HomeView.vue
@@ -0,0 +1,45 @@
+<!-- HTML BLOCK -->
+<template>
+  <div class="container">
+
+    <!--display heading of the page>--> 
+
+    <h2>Welcome to Slice & Spice</h2>
+    <h3>Savor the Slice, Taste the Tradition</h3>
+
+    <!--display the image of the restaurant>--> 
+
+    <img src="/src/assets/pizzeria.jpg" alt="Pizzeria picture" class="pizzeria-image">
+  </div>
+</template>
+
+
+<!-- CSS BLOCK -->
+<style>
+
+.container {
+text-align: center;
+padding: 20px;
+background-color: #fff; 
+border-radius: 10px;
+
+}
+
+h2 {
+font-size: 24px;
+color: #333; 
+margin-bottom: 20px;
+}
+
+/* Apply styles to the image */
+.pizzeria-image {
+max-width: 70%;
+height: auto;
+border-radius: 10px;
+box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2); 
+}
+</style>
+
+
+
+
